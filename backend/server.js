19fbebbc6355c8d01491acc535a6c02d529404dd
rui-saito <rui_saito@mail.toyota.co.jp>
2023-06-02 15:17:58 +0900

    origin追加

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,17 @@ const PORT = 8080;
 
 app.use(express.json());
 
+const allowed_origins = [
+  "https://bitcruiser.onrender.com", "http://localhost:3000"
+];
+const origin = req.headers.Origin;
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", ["https://bitcruiser.onrender.com", "http://localhost:3000"]);
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  next();
+  if (allowed_origins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+  }
 });
 
 /**
