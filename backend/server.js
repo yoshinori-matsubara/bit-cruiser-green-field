8d460d1799d427806325cb46c9f767a09ac4a29d
rui-saito <rui_saito@mail.toyota.co.jp>
2023-06-02 14:16:15 +0900

    ヘッダー変更

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = 8080;
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://bitcruiser.onrender.com");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "*");
   next();
