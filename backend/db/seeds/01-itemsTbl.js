6d2563cf0d6e5da9ab160ae835abc4805cfd8d82
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-06-05 10:42:03 +0900

    seed修正

--- backend/db/seeds/01-itemsTbl.js
+++ backend/db/seeds/01-itemsTbl.js
@@ -4,8 +4,8 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
+  await knex('items').del()
+  await knex('items').insert([
     { id: 1, item_name: 'トイレットペーパー', item_stock: '2', consumption_day: '25', notification_stock: '1', registration_day: '2023-05-01' },
     { id: 1, item_name: '洗濯洗剤', item_stock: '2', consumption_day: '20', notification_stock: '1', registration_day: '2023-05-01' },
     { id: 1, item_name: 'シーチキン缶詰', item_stock: '5', consumption_day: '10', notification_stock: '1', registration_day: '2023-05-01' },
